a84d2cacec4cb91f0555b588acd55a6912fe677d
fhr3n <ridhofahren@gmail.com>
2020-03-22 18:15:30 +0700

    second commit

---
+++ TugasClassDanObject/TugasClassDanObject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassDanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Fahren";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+          
+        }
+    }
+}
---
+++ TugasClassDanObject/TugasClassDanObject/Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassDanObject
+{
+    class Taxi
+    {
+        public string DriverName;
+        public Boolean OnDuty;
+        public float NumPassenger;
+
+      
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", OnDuty);
+            Console.WriteLine("Number Of Passenger: {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
+
+
